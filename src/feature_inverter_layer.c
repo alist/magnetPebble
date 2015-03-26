b837bee443b07de46a9a2772a13ebef5f5674581
Alexander List <atlex2@gmail.com>
2015-03-26 05:14:24 -0700

    added time to bottom

--- src/feature_inverter_layer.c
+++ src/feature_inverter_layer.c
@@ -7,29 +7,26 @@
 
 
 static Window *s_main_window;
-static TextLayer *s_text_below, *s_text_above;
 static InverterLayer *s_inverter_layer;
+static TextLayer *s_time_layer;
+
 uint64_t lastTapTime = 0;
 
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_frame(window_layer);
 
-  s_text_below = text_layer_create(GRect(0, 54, bounds.size.w, bounds.size.h-54 /* height */));
-  text_layer_set_text(s_text_below, "below");
-  text_layer_set_font(s_text_below, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
-  text_layer_set_text_alignment(s_text_below, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(s_text_below));
+  // Create time TextLayer
+  s_time_layer = text_layer_create(GRect(0, 108, 144, 50));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 
-  s_inverter_layer = inverter_layer_create(GRect(bounds.size.w/2, 0, bounds.size.w/2, bounds.size.h));
+  s_inverter_layer = inverter_layer_create(GRect(0, -60, bounds.size.w, bounds.size.h));
   layer_add_child(window_layer, inverter_layer_get_layer(s_inverter_layer));
-
-  s_text_above = text_layer_create(GRect(0, 84, bounds.size.w, 168-84 /* height */));
-  text_layer_set_text(s_text_above, "above");
-  text_layer_set_font(s_text_above, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
-  text_layer_set_text_alignment(s_text_above, GTextAlignmentCenter);
-  text_layer_set_background_color(s_text_above, GColorClear);
-  layer_add_child(window_layer, text_layer_get_layer(s_text_above));
 }
 
 static void flipLayer(){
@@ -88,8 +85,7 @@ static void timer_callback(void *data) {
 }
 
 static void main_window_unload(Window *window) {
-  text_layer_destroy(s_text_above);
-  text_layer_destroy(s_text_below);
+  text_layer_destroy(s_time_layer);
   inverter_layer_destroy(s_inverter_layer);
 }
 
@@ -117,7 +113,7 @@ static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResul
 }
 
 static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
-  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+  // APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
 
 static void init() {
