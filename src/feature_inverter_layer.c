d6b0e552df877da6d99c0605689951707a1a9b00
Alexander List <atlex2@gmail.com>
2015-03-29 14:47:03 -0700

    did I mention it's black?

--- src/feature_inverter_layer.c
+++ src/feature_inverter_layer.c
@@ -32,7 +32,7 @@ static void main_window_load(Window *window) {
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 
-  s_image_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BLACK_PEBBLE_MAGNET_2);
+  s_image_bitmap = gbitmap_create_with_resource(RESOURCE_ID_PEBBLE_MAGNET_2_BLACK);
   GRect imageDisplayRect = GRect(0, 0, 144, 110);
     // Use GCompOpClear to display the black portions of the image
   s_image_layer = bitmap_layer_create(imageDisplayRect);
